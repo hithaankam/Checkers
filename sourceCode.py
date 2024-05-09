0d214a9d5b72a4ab24d28b5356728caa865a092a
hithaankam <23wh1a1289@bvrithyderabad.edu.in>
2024-05-09 17:25:25 +0530

    Created panel

---
+++ sourceCode.py
@@ -0,0 +1,19 @@
+from tkinter import * 
+
+
+board_root = Tk()
+
+# Set the title of the window
+board_root.title("CHECKERS")
+
+
+
+# Width X Height
+board_root.geometry("744x634")
+
+# Minimum width, height
+board_root.minsize(500, 500)
+
+# GUI logic here
+
+board_root.mainloop()
